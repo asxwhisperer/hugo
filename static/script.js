2098755d530d994c7ad7314976a2e4acce87f793
asxwhisperer <asxwhisperer@protonmail.com>
2023-04-28 21:03:22 +1000

    update

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
-// Get all ".post-content" elements and "h1" elements
-const elements = document.querySelectorAll('.post-content, h1');
+// Get all ".post-content" elements and ".title-link" elements
+const elements = document.querySelectorAll('.post-content, .title-link');
 
 // Loop through each element and search for the desired strings
 elements.forEach((el) => {
@@ -10,11 +10,12 @@ elements.forEach((el) => {
   // Loop through each match and wrap it in a "span" or "a" tag with the class "ticker"
   let modifiedContent = content;
   for (const match of matches) {
-    const tickerEl = document.createElement(el.tagName === 'H1' ? 'span' : 'a');
+    const tickerEl = document.createElement(el.classList.contains('title-link') ? 'span' : 'a');
     tickerEl.classList.add('ticker');
     const url = `/categories/${match[1].split(':')[1].trim().toLowerCase()}/`; // Get the URL from the matched regex and ensure it's lowercase
-    if (el.tagName !== 'H1') { // If it's an "a" tag, set the href attribute
+    if (!el.classList.contains('title-link')) { // If it's an "a" tag, set the href attribute and onclick attribute
       tickerEl.href = url;
+      tickerEl.setAttribute('onclick', "fathom.trackGoal('H79GZHC5', 0)"); // Add onclick attribute using setAttribute
     }
     tickerEl.innerHTML = `(${match[1]})`;
     modifiedContent = modifiedContent.replace(match[0], tickerEl.outerHTML);
